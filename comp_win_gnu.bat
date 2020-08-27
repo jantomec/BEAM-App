f68a08c8eaf9717683bab92e25e58586d71569a2
Jan Tomec <jan.tomec@gradri.uniri.hr>
2020-08-27 13:58:35 +0200

    Move scripts to a new folder

--- comp_win_gnu.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Create lib folder
-mkdir lib
-mkdir lapack\lib
-:: Build LAPACK routines
-for %%f in (lapack/lapack_routine/*.f) do gfortran -c lapack/lapack_routine/%%f -o lapack/lib/%%~nf.o
-:: Build FEANBEAM modules
-for %%f in (source/*.f90) do gfortran -c source/%%f -Jlib -o lib/%%~nf.o
