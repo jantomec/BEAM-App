5a974aff848150d16ca0c7905867e222f1e06020
Jan Tomec <jan.tomec@gmail.com>
2020-11-06 13:22:23 +0100

    Compile and build script for macOS

--- scripts/build_mac_gnu.zsh
+++ scripts/build_mac_gnu.zsh
@@ -2,7 +2,34 @@
 
 if [[ $1 == "" ]]
 then
-    echo "No filename. Correct usage: build_mac_gnu.zsh filename.f90"
+    echo "\nNo filename. Correct usage: build_mac_gnu.zsh filename.f90\n"
     exit
 fi
-echo $1
+
+file="${1##*/}"
+filename="${file%.f90}"
+
+echo "\nLoading LAPACK libraries\n"
+LIST2=""
+for file in lapack/lib/*.o
+do
+  echo "Loading" $file
+  LIST2=$LIST2" "$file
+done
+
+echo "\n============================\n"
+echo "Loading BEAM App libraries\n"
+LIST1=""
+for file in lib/*.o
+do
+  echo "Loading" $file
+  LIST1=$LIST1" "$file
+done
+
+echo "\n============================\n"
+echo "Building program " $1
+comm="gfortran -Ilib -Ilapack/lib "$1$LIST2$LIST1" -o "$filename
+
+eval $comm
+
+echo "Finished building program\n"
--- scripts/comp_mac_gnu.zsh
+++ scripts/comp_mac_gnu.zsh
@@ -1,17 +1,24 @@
 #!/bin/zsh
 
 # Create lib folder
+echo "\nCreating directories"
 mkdir lib
 mkdir lapack/lib
 
 # Build LAPACK routines
+echo "\n============================\n"
+echo "Building LAPACK libraries\n"
 for file in lapack/lapack_routine/*.f(:t:r); do
+  echo "Building" $file.f
   gfortran -c lapack/lapack_routine/$file.f -o lapack/lib/$file.o
 done
 
-# Build FEANBEAM modules
+# Build BEAM App modules
+echo "\n============================\n"
+echo "Building BEAM App libraries\n"
 for file in source/*.f90(:t:r); do
+  echo "Building" $file.f90
   gfortran -c source/$file.f90 -Jlib -o lib/$file.o
 done
-
-echo "Finished compiling libraries"
+echo "\n============================\n"
+echo "Finished compiling libraries\n"
